f2a48d8b6e7ca282c93fbf77f97ea63ecd5fa677
Raul r <prueba@correo.com>
2026-02-10 19:44:47 -0300

    Add client email ro.guajardo.vega@gmail.com to TO_EMAIL recipients

--- public/contacto-config.php
+++ public/contacto-config.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 return [
   'BASE_PATH' => '',
   'SITE_URL' => 'https://protrabajo.cl',
-  'TO_EMAIL' => 'codigoraul@gmail.com',
+  'TO_EMAIL' => 'codigoraul@gmail.com, ro.guajardo.vega@gmail.com',
   'FROM_EMAIL' => 'contacto@protrabajo.cl',
   'FROM_NAME' => 'ProTrabajo',
   'BCC_EMAILS' => '',
