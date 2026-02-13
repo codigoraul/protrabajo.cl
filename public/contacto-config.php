77ca24e991ccb184efa2e908a8e6f62fc9358dfd
Raul r <prueba@correo.com>
2026-02-13 12:42:49 -0300

    Add contacto@polerasfutbol.cl as 4th recipient for testing

--- public/contacto-config.php
+++ public/contacto-config.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 return [
   'BASE_PATH' => '',
   'SITE_URL' => 'https://protrabajo.cl',
-  'TO_EMAIL' => 'codigoraul@gmail.com, contacto@protrabajo.cl, rguajardo@protrabajo.cl',
+  'TO_EMAIL' => 'codigoraul@gmail.com, contacto@protrabajo.cl, rguajardo@protrabajo.cl, contacto@polerasfutbol.cl',
   'FROM_EMAIL' => 'contacto@protrabajo.cl',
   'FROM_NAME' => 'ProTrabajo',
   'BCC_EMAILS' => '',
