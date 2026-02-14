40f4822d61c0430f62f9f1736ca2b70a9688fcbe
Raul r <prueba@correo.com>
2026-02-13 21:10:31 -0300

    Fix: Clean up contacto-config.php - remove duplicate FROM_EMAIL lines

--- public/contacto-config.php
+++ public/contacto-config.php
@@ -1,12 +1,12 @@
-'formulario@protrabajo.cl'
-'formulario@protrabajo.cl'
-'formulario@protrabajo.cl'
-'formulario@protrabajo.cl'
-'formulario@protrabajo.cl'
-'formulario@protrabajo.cl'
-'formulario@protrabajo.cl'
-'formulario@protrabajo.cl'
-'formulario@protrabajo.cl'
-'formulario@protrabajo.cl'
-'formulario@protrabajo.cl'
-'formulario@protrabajo.cl'
+<?php
+
+declare(strict_types=1);
+
+return [
+  'BASE_PATH' => '',
+  'SITE_URL' => 'https://protrabajo.cl',
+  'TO_EMAIL' => 'codigoraul@gmail.com, contacto@protrabajo.cl, rguajardo@protrabajo.cl, contacto@polerasfutbol.cl',
+  'FROM_EMAIL' => 'formulario@protrabajo.cl',
+  'FROM_NAME' => 'ProTrabajo',
+  'BCC_EMAILS' => '',
+];
